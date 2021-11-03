6038d279b0401c22ab223c84a85270e4357052d3
fredyrios22 <fredy.rios@udea.edu.co>
2021-11-03 16:56:36 -0500

    agregar títulos en paginas y _docment para tener iconos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('estoy en el app');
   return <Component {...pageProps} />
 }
 
